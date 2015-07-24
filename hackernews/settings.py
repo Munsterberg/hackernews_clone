af8a7f11ac3bfe29bfb735ef3b8227b738041fb9
Munsterberg <jetrussell93@gmail.com>
2015-07-24 08:06:01 -0400

    Deployment

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'hackernews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hackernews',
-        'USER': 'munsterberg',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'hackernews',
+#         'USER': 'munsterberg',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config(default='postgres://tsoivsmjerfzck:cBL9VxTIgvGO7QHS0Q8iRFaF6L@ec2-54-83-55-214.compute-1.amazonaws.com:5432/d7oa7237faoobc')
